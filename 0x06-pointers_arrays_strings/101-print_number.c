b10fd9c99a5358845bd4cd728b8540e9f6d0d12d
Iqra Abdifitax Hussein <98606326+IQRA-ABDI@users.noreply.github.com>
2023-03-02 22:51:59 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,50 +1,21 @@
 #include "main.h"
 /**
- * print_number - print integer with putchar
- *
- * @n: function print integer n
- *
- * Return: none
+ * print_number - print numbers chars
+ * @n: integer params
+ * Return: 0
  */
 void print_number(int n)
 {
-	int i;
-	int j;
-	int k;
-	unsigned int l;
-	unsigned int m;
-
-	j = 1;
-	k = 0;
-	l = n;
-	m = n;
-	if (n == 0)
-	{
-		_putchar(48);
-	}
-	if (n < 0)
-	{
-		_putchar(45);
-		n = n + 1;
-		n = -n;
-		m = n;
-		l = n;
-		l = l + 1;
-		m = m + 1;
-	}
-	while (l != 0)
-	{
-		l = (l / 10);
-		k++;
-	}
-	for (i = 1; i < k; i++)
-	{
-		j = (j * 10);
-	}
-	for (i = 0; i < k; i++)
-	{
-		_putchar((m / j) + 48);
-		m = (m % j);
-		j = (j / 10);
-	}
+        unsigned int n1;
+        n1 = n;
+        if (n < 0)
+        {
+                _putchar('-');
+                n1 = -n;
+        }
+        if (n1 / 10 != 0)
+        {
+                print_number(n1 / 10);
+        }
+        _putchar((n1 % 10) + '0');
 }
